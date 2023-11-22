90b6dd6c52cd61a425b6b01ba3cd768d35bca4c6
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2023-11-22 14:32:02 +0700

    Table class added to database.py

--- database.py
+++ database.py
@@ -32,7 +32,57 @@ class DB:
                 return table
         return None
 # add in code for a Table class
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
 
+    def join(self, other_table, common_key):
+        joined_table = Table(
+            self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+
+    def insert(self, new_table):
+        self.table.append(new_table)
+
+    def update(self, user_id, key, value):
+        for i in self.table:
+            user_id_key = list(i.keys())[0]
+            if i[user_id_key] == user_id:
+                i[key] = value
+
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            temps.append(float(item1[aggregation_key]))
+        return function(temps)
+
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
 # modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
