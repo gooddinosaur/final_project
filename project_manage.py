34a572c1904752afbea3b9b30069049cde3c0d67
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2023-11-24 02:09:43 +0700

    little draft code for check_role and initializing function in project_manage.py

--- project_manage.py
+++ project_manage.py
@@ -1,7 +1,10 @@
 # import database module
 from database import Read, DB, Table
+import csv
 
 my_DB = DB()
+
+
 # define a funcion called initializing
 
 def initializing():
@@ -10,7 +13,6 @@ def initializing():
     Read.insert(read_person)
     Read.insert(read_login)
     persons_table = Table('persons', read_person.info)
-    my_DB.insert(persons_table)
     login_table = Table('login', [])
     for i in range(len(persons_table.table)):
         if persons_table.table[i]['type'] == 'admin':
@@ -24,19 +26,30 @@ def initializing():
                                         persons_table.table[i]['last'][0],
                             'password': read_login.info[i]['password'],
                             'role': role})
-        my_DB.insert(login_table)
+    project_table = Table('project', [])
+    #for x in range(len(num_project):
+        #project_table.insert({})
+    Advisor_pending_request_table = Table('Advisor_pending_request', [])
+    Member_pending_request_table = Table('Member_pending_request', [])
+    my_DB.insert(persons_table)
+    my_DB.insert(login_table)
+    my_DB.insert(project_table)
+    my_DB.insert(Advisor_pending_request_table)
+    my_DB.insert(Member_pending_request_table)
     print(persons_table)
     print(login_table)
+    print(my_DB.search('persons'))
+
 
 # here are things to do in this function:
 
-    # create an object to read all csv files that will serve as a persistent state for this program
+# create an object to read all csv files that will serve as a persistent state for this program
 
-    # create all the corresponding tables for those csv files
+# create all the corresponding tables for those csv files
 
-    # see the guide how many tables are needed
+# see the guide how many tables are needed
 
-    # add all these tables to the database
+# add all these tables to the database
 
 
 # define a funcion called login
@@ -50,20 +63,22 @@ def login():
             return [i['person_id'], i['role']]
     return None
 
+
 # here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [ID, role] if valid, otherwise returning None
+# add code that performs a login task
+# ask a user for a username and password
+# returns [ID, role] if valid, otherwise returning None
 
 # define a function called exit
 def exit():
     pass
 
+
 # here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-   
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+# write out all the tables that have been modified to the corresponding csv files
+# By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
+
+# https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
 
 
 # make calls to the initializing and login functions defined above
@@ -74,20 +89,31 @@ print(val)
 person_table = my_DB.search('persons')
 person_table.update('7447677', 'type', 'test')
 print(person_table)
+
+
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
-# if val[1] = 'admin':
+def check_role(val):
+    if val[1] == 'admin':
+        pass
     # see and do admin related activities
-# elif val[1] = 'student':
+    elif val[1] == 'student':
+        pass
     # see and do student related activities
-# elif val[1] = 'member':
+    elif val[1] == 'member':
+        pass
     # see and do member related activities
-# elif val[1] = 'lead':
+    elif val[1] == 'lead':
+        pass
     # see and do lead related activities
-# elif val[1] = 'faculty':
+    elif val[1] == 'faculty':
+        pass
     # see and do faculty related activities
-# elif val[1] = 'advisor':
-    # see and do advisor related activities
+    elif val[1] == 'advisor':
+        pass
+
+
+# see and do advisor related activities
 
 # once everyhthing is done, make a call to the exit function
 exit()
