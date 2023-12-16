8609debcfce54174b14fe7f56a0c653a17c56c66
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2023-12-17 00:18:06 +0700

    finished student, member,lead case in check role function in project_manage.py

--- project_manage.py
+++ project_manage.py
@@ -1,6 +1,6 @@
 # import database module
 from database import Read, DB, Table
-from role import admin
+from role import admin, student, member, lead
 import csv
 
 my_DB = DB()
@@ -16,20 +16,15 @@ def initializing():
     persons_table = Table('persons', read_person.info)
     login_table = Table('login', [])
     for i in range(len(persons_table.table)):
-        if persons_table.table[i]['type'] == 'admin':
-            role = "Admin"
-        elif persons_table.table[i]['type'] == 'student':
-            role = "Member"
-        elif persons_table.table[i]['type'] == 'faculty':
-            role = "Faculty"
+        role = persons_table.table[i]['type']
         login_table.insert({'person_id': persons_table.table[i]['ID'],
                             'username': persons_table.table[i]['fist'] + "." +
                                         persons_table.table[i]['last'][0],
                             'password': read_login.info[i]['password'],
                             'role': role})
-    project_table = Table('project', [])
-    #for x in range(len(num_project):
-        #project_table.insert({})
+    project_table = Table('Project', [])
+    # for x in range(len(num_project):
+    # project_table.insert({})
     Advisor_pending_request_table = Table('Advisor_pending_request', [])
     Member_pending_request_table = Table('Member_pending_request', [])
     my_DB.insert(persons_table)
@@ -39,7 +34,6 @@ def initializing():
     my_DB.insert(Member_pending_request_table)
     print(persons_table)
     print(login_table)
-
 
 
 # here are things to do in this function:
@@ -65,17 +59,30 @@ def login():
             return [i['person_id'], i['role']]
     return None
 
+
 def exit():
-    pass
+    for table in my_DB.database:
+        file = open(table.table_name, 'w')
+        writer = csv.writer(file)
+        keys = []
+        if len(table.table) >= 1:
+            for key in table.table[0].keys():
+                keys.append(key)
+            writer.writerow(keys)
+            for dict in table.table:
+                writer.writerow(dict.values())
+        file.close()
 
 
 # here are things to do in this function:
 # write out all the tables that have been modified to the corresponding csv files
-# By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
+# By now, you know how to read in a csv file and transform it into a list of dictionaries.
+# For this project, you also need to know how to do the reverse, i.e.,
+# writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
 
 # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
-def check_role(val): #val 0 is id, val 1 is roles
-    if val[1] == 'Admin' or val[1] == 'admin':
+def check_role(val):  # val 0 is id, val 1 is roles
+    if val[1] == 'admin':
         person = admin(val[0], my_DB)
         choice = admin.ask_need(person)
         if choice == 1:
@@ -84,18 +91,40 @@ def check_role(val): #val 0 is id, val 1 is roles
             admin.manage_database(person)
         elif choice == 3:
             admin.manage_table(person)
-
-
-
     # see and do admin related activities
     elif val[1] == 'student':
-        pass
+        person = student(val[0], my_DB)
+        choice = student.ask_need(person)
+        if choice == 1:
+            student.response_request(person)
+        elif choice == 2:
+            student.create_project(person)
     # see and do student related activities
-    elif val[1] == 'Member':
-        pass
+    elif val[1] == 'member':
+        person = member(val[0], my_DB)
+        choice = member.ask_need(person)
+        if choice == 1:
+            member.see_proj_status(person)
+        elif choice == 2:
+            member.see_modified(person)
+        elif choice == 3:
+            member.see_response(person)
     # see and do member related activities
     elif val[1] == 'lead':
-        pass
+        person = lead(val[0], my_DB)
+        choice = lead.ask_need(person)
+        if choice == 1:
+            lead.see_proj_status(person)
+        elif choice == 2:
+            lead.see_modified(person)
+        elif choice == 3:
+            lead.see_response(person)
+        elif choice == 4:
+            lead.find_member(person)
+        elif choice == 5:
+            lead.find_advisor(person)
+        elif choice == 6: #Submit project
+            pass
     # see and do lead related activities
     elif val[1] == 'faculty':
         pass
@@ -108,13 +137,7 @@ def check_role(val): #val 0 is id, val 1 is roles
 
 initializing()
 val = login()
-print(val)
 check_role(val)
-
-
-
-
-
 
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 # see and do advisor related activities
